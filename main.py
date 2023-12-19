fde8912ee425dc6136cdc61dd35ebd456f607541
Artem Batko <ab2009ukr@gmail.com>
2023-12-19 18:32:36 +0000

    A sample message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator.")
+print("Welcome to the Band Name Generator!")
 # street = input("What's the name of the city you grew up in?\n")
 # pet = input("What's your first pet's name?\n")
 # print("Your band cool name could be " + street + " " + pet)
